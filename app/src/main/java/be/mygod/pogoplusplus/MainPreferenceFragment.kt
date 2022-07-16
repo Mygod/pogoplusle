bad80b71e5ca9800c6ca4e177ea7a18ac188057d
Mygod <contact-git@mygod.be>
2022-07-16 18:38:13 -0400

    First draft of settings

---
+++ app/src/main/java/be/mygod/pogoplusplus/MainPreferenceFragment.kt
@@ -0,0 +1,54 @@
+package be.mygod.pogoplusplus
+
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.os.Build
+import android.os.Bundle
+import android.provider.Settings
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.preference.Preference
+import androidx.preference.PreferenceFragmentCompat
+import androidx.preference.TwoStatePreference
+import com.google.android.gms.oss.licenses.OssLicensesMenuActivity
+
+class MainPreferenceFragment : PreferenceFragmentCompat() {
+    private lateinit var permissionBluetooth: TwoStatePreference
+    private fun Preference.remove() = parent!!.removePreference(this)
+
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        addPreferencesFromResource(R.xml.pref_main)
+        permissionBluetooth = findPreference("permission.bluetooth")!!
+        if (Build.VERSION.SDK_INT >= 31) permissionBluetooth.setOnPreferenceChangeListener { _, newValue ->
+            if (newValue as Boolean) requestBluetoothPermission.launch(Manifest.permission.BLUETOOTH_CONNECT) else {
+                startActivity(Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS).apply {
+                    data = Uri.fromParts("package", requireContext().packageName, null)
+                })
+            }
+            false
+        } else permissionBluetooth.remove()
+        findPreference<Preference>("permission.notification")!!.apply {
+            setOnPreferenceClickListener {
+                startActivity(Intent(Settings.ACTION_APP_NOTIFICATION_SETTINGS).apply {
+                    putExtra(Settings.EXTRA_APP_PACKAGE, requireContext().packageName)
+                })
+                true
+            }
+        }
+        findPreference<Preference>("misc.licenses")!!.setOnPreferenceClickListener {
+            startActivity(Intent(context, OssLicensesMenuActivity::class.java))
+            true
+        }
+    }
+
+    private val requestBluetoothPermission = registerForActivityResult(ActivityResultContracts.RequestPermission()) {
+        permissionBluetooth.isChecked = it
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if (Build.VERSION.SDK_INT >= 31) permissionBluetooth.isChecked = requireContext().checkSelfPermission(
+            Manifest.permission.BLUETOOTH_CONNECT) == PackageManager.PERMISSION_GRANTED
+    }
+}
