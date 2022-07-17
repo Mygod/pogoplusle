77c646b33766a2bdeea605335966d0cb2da18830
Mygod <contact-git@mygod.be>
2022-07-17 11:37:34 -0400

    Add consent dialog

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "be.mygod.pogoplusplus"
         minSdk = 24
         targetSdk = 33
-        versionCode = 1
-        versionName = "0.0.1"
+        versionCode = 2
+        versionName = "0.0.2"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         resourceConfigurations.addAll(arrayOf("en"))
         buildConfigField("boolean", "DONATIONS", "true")
--- app/src/main/java/be/mygod/pogoplusplus/MainActivity.kt
+++ app/src/main/java/be/mygod/pogoplusplus/MainActivity.kt
@@ -6,6 +6,7 @@ import android.provider.Settings
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.WindowCompat
 import be.mygod.pogoplusplus.databinding.ActivityMainBinding
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -16,8 +17,14 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         setSupportActionBar(binding.toolbar)
-        binding.serviceSwitch.setOnCheckedChangeListener { _, _ ->
-            startActivity(Intent(Settings.ACTION_ACCESSIBILITY_SETTINGS))
+        binding.serviceSwitch.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) MaterialAlertDialogBuilder(this).apply {
+                binding.serviceSwitch.isChecked = false
+                setTitle("Grant control to this app?")
+                setMessage("This app needs to read and control notifications and screen for system Settings and the game in order to skip pairing dialog and more. No data is collected through this process except for crash logs.\n\nTo grant the permission, click 'Agree', then click on this app and turn on the main switch that says 'Use PoGo+LE'.")
+                setNegativeButton("Reject", null)
+                setPositiveButton("Accept") { _, _ -> startActivity(Intent(Settings.ACTION_ACCESSIBILITY_SETTINGS)) }
+            }.create().show() else startActivity(Intent(Settings.ACTION_ACCESSIBILITY_SETTINGS))
         }
     }
 
