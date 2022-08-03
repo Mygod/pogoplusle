30cf1b3fc86ffa17d0f4e6e8c9bffbc6cd3644b7
Mygod <contact-git@mygod.be>
2022-08-03 00:03:56 -0400

    Update dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "be.mygod.pogoplusplus"
         minSdk = 24
         targetSdk = 33
-        versionCode = 7
-        versionName = "0.1.0"
+        versionCode = 8
+        versionName = "0.1.1"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         buildConfigField("boolean", "DONATIONS", "true")
     }
@@ -58,13 +58,13 @@ dependencies {
     implementation("androidx.browser:browser:1.4.0")
     implementation("androidx.core:core-ktx:1.8.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.5.0")    // TODO fix dependency
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.5.1")    // TODO fix dependency
     implementation("androidx.preference:preference:1.2.0")
     implementation("com.android.billingclient:billing-ktx:5.0.0")
     implementation("com.google.android.gms:play-services-oss-licenses:17.0.0")
     implementation("com.google.android.material:material:1.7.0-alpha03")
     implementation("com.google.firebase:firebase-analytics-ktx:21.1.0")
-    implementation("com.google.firebase:firebase-crashlytics:18.2.11")
+    implementation("com.google.firebase:firebase-crashlytics:18.2.12")
     implementation("com.jakewharton.timber:timber:5.0.1")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test:runner:1.4.0")
