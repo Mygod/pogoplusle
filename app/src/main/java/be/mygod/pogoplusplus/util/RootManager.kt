c6cd1bef3dc249ee0aa52884c7c2df624ed7063c
Mygod <contact-git@mygod.be>
2022-08-31 16:06:33 -0400

    Fix BluetoothAdapter being null on Android 13+

--- app/src/main/java/be/mygod/pogoplusplus/util/RootManager.kt
+++ app/src/main/java/be/mygod/pogoplusplus/util/RootManager.kt
@@ -1,11 +1,31 @@
 package be.mygod.pogoplusplus.util
 
+import android.annotation.SuppressLint
+import android.os.Build
+import android.os.Parcelable
+import androidx.annotation.RequiresApi
+import be.mygod.librootkotlinx.RootCommandNoResult
 import be.mygod.librootkotlinx.RootServer
 import be.mygod.librootkotlinx.RootSession
 import be.mygod.pogoplusplus.App.Companion.app
+import kotlinx.parcelize.Parcelize
 import java.util.concurrent.TimeUnit
 
 object RootManager : RootSession() {
+    @Parcelize
+    @RequiresApi(33)
+    class RootInit : RootCommandNoResult {
+        @SuppressLint("BlockedPrivateApi", "PrivateApi")
+        override suspend fun execute(): Parcelable? {
+            // this is needed for later using BluetoothAdapter
+            Class.forName("android.app.ActivityThread").getDeclaredMethod("initializeMainlineModules").invoke(null)
+            return null
+        }
+    }
+
     override val timeout get() = TimeUnit.MINUTES.toMillis(1)
-    override suspend fun initServer(server: RootServer) = server.init(app)
+    override suspend fun initServer(server: RootServer) {
+        server.init(app)
+        if (Build.VERSION.SDK_INT >= 33) server.execute(RootInit())
+    }
 }
