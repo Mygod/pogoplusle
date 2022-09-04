2f539a1e8450b0cdbd03f515780772766c7d7703
Mygod <contact-git@mygod.be>
2022-09-04 15:19:47 -0400

    v0.2.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "be.mygod.pogoplusplus"
         minSdk = 24
         targetSdk = 33
-        versionCode = 11
-        versionName = "0.2.0"
+        versionCode = 12
+        versionName = "0.2.1"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         buildConfigField("boolean", "DONATIONS", "true")
     }
@@ -65,8 +65,8 @@ dependencies {
     implementation("com.android.billingclient:billing-ktx:5.0.0")
     implementation("com.google.android.gms:play-services-oss-licenses:17.0.0")
     implementation("com.google.android.material:material:1.7.0-rc01")
-    implementation("com.google.firebase:firebase-analytics-ktx:21.1.0")
-    implementation("com.google.firebase:firebase-crashlytics:18.2.12")
+    implementation("com.google.firebase:firebase-analytics-ktx:21.1.1")
+    implementation("com.google.firebase:firebase-crashlytics:18.2.13")
     implementation("com.jakewharton.timber:timber:5.0.1")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test:runner:1.4.0")
