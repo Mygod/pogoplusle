e8f2ec7461189b40ad71a4742a8ef6123e1d5043
Mygod <contact-git@mygod.be>
2022-09-06 10:00:17 -0400

    v1.0.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "be.mygod.pogoplusplus"
         minSdk = 24
         targetSdk = 33
-        versionCode = 14
-        versionName = "0.2.2"
+        versionCode = 15
+        versionName = "1.0.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         buildConfigField("boolean", "DONATIONS", "true")
     }
