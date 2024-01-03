a2d520f6c632323cd370c7b2e3a8195479412cdf
Mygod <contact-git@mygod.be>
2024-01-03 11:00:04 -0500

    Update dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -78,7 +78,7 @@ dependencies {
     implementation("be.mygod.librootkotlinx:librootkotlinx:1.1.1")
     implementation("com.android.billingclient:billing-ktx:6.1.0")
     implementation("com.google.android.gms:play-services-oss-licenses:17.0.1")
-    implementation("com.google.android.material:material:1.11.0-rc01")
+    implementation("com.google.android.material:material:1.11.0")
     implementation("com.google.firebase:firebase-analytics:21.5.0")
     implementation("com.google.firebase:firebase-crashlytics:18.6.0")
     implementation("com.jakewharton.timber:timber:5.0.1")
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,9 @@
 plugins {
-    id("com.android.application") version "8.3.0-alpha16" apply false
+    id("com.android.application") version "8.3.0-beta01" apply false
     id("com.github.ben-manes.versions") version "0.50.0"
     id("com.google.firebase.crashlytics") version "2.9.9" apply false
     id("com.google.gms.google-services") version "4.4.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.20" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
 }
 
 buildscript {
