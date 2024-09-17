6cf9d300ab85d81f2e7407d48b6591e2b08a2b68
Mygod <contact-git@mygod.be>
2024-09-17 14:25:30 -0700

    Update dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,11 +12,11 @@ plugins {
 android {
     namespace = "be.mygod.pogoplusplus"
     buildToolsVersion = "34.0.0"
-    compileSdk = 34
+    compileSdk = 35
     defaultConfig {
         applicationId = "be.mygod.pogoplusplus"
         minSdk = 28
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 34
         versionName = "1.3.7"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
@@ -70,19 +70,19 @@ android {
 }
 
 dependencies {
-    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.4")
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.2")
     implementation("androidx.browser:browser:1.8.0")
-    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.preference:preference:1.2.1")
-    implementation("be.mygod.librootkotlinx:librootkotlinx:1.1.1")
-    implementation("com.android.billingclient:billing-ktx:6.2.0")
-    implementation("com.google.android.gms:play-services-oss-licenses:17.0.1")
-    implementation("com.google.android.material:material:1.11.0")
-    implementation("com.google.firebase:firebase-analytics:21.6.1")
-    implementation("com.google.firebase:firebase-crashlytics:18.6.3")
+    implementation("be.mygod.librootkotlinx:librootkotlinx:1.2.0")
+    implementation("com.android.billingclient:billing-ktx:7.0.0")
+    implementation("com.google.android.gms:play-services-oss-licenses:17.1.0")
+    implementation("com.google.android.material:material:1.12.0")
+    implementation("com.google.firebase:firebase-analytics:22.1.0")
+    implementation("com.google.firebase:firebase-crashlytics:19.1.0")
     implementation("com.jakewharton.timber:timber:5.0.1")
     testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test:runner:1.5.2")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation("androidx.test:runner:1.6.2")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,9 @@
 plugins {
-    id("com.android.application") version "8.3.1" apply false
+    id("com.android.application") version "8.6.0" apply false
     id("com.github.ben-manes.versions") version "0.51.0"
-    id("com.google.firebase.crashlytics") version "2.9.9" apply false
-    id("com.google.gms.google-services") version "4.4.1" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.23" apply false
+    id("com.google.firebase.crashlytics") version "3.0.2" apply false
+    id("com.google.gms.google-services") version "4.4.2" apply false
+    id("org.jetbrains.kotlin.android") version "2.0.20" apply false
 }
 
 buildscript {
