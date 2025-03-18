473920b69565d3e4b31650313b88efadf9f9a589
Mygod <contact-git@mygod.be>
2025-03-17 17:37:05 -0700

    Update dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,7 @@ plugins {
 
 android {
     namespace = "be.mygod.pogoplusplus"
-    buildToolsVersion = "34.0.0"
-    compileSdk = 35
+    compileSdk = 36
     defaultConfig {
         applicationId = "be.mygod.pogoplusplus"
         minSdk = 28
@@ -70,17 +69,17 @@ android {
 }
 
 dependencies {
-    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.3")
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.5")
     implementation("androidx.browser:browser:1.8.0")
     implementation("androidx.core:core-ktx:1.15.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.2.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.2.1")
     implementation("androidx.preference:preference:1.2.1")
     implementation("be.mygod.librootkotlinx:librootkotlinx:1.2.1")
     implementation("com.android.billingclient:billing-ktx:7.1.1")
     implementation("com.google.android.gms:play-services-oss-licenses:17.1.0")
     implementation("com.google.android.material:material:1.12.0")
-    implementation("com.google.firebase:firebase-analytics:22.1.2")
-    implementation("com.google.firebase:firebase-crashlytics:19.3.0")
+    implementation("com.google.firebase:firebase-analytics:22.3.0")
+    implementation("com.google.firebase:firebase-crashlytics:19.4.1")
     implementation("com.jakewharton.timber:timber:5.0.1")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test:runner:1.6.2")
