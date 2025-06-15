20d78c1ddefe1baa6f4764681f399d6476928d6a
Mygod <contact-git@mygod.be>
2025-06-14 20:06:37 -0700

    Update dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,7 +16,7 @@ android {
     defaultConfig {
         applicationId = "be.mygod.pogoplusplus"
         minSdk = 28
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 39
         versionName = "1.3.11"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
@@ -76,16 +76,15 @@ dependencies {
     implementation(libs.material3.android)
     implementation("androidx.activity:activity-compose:1.10.1")
     implementation("androidx.browser:browser:1.8.0")
-    implementation("androidx.compose.foundation:foundation-layout:1.8.1")
+    implementation("androidx.compose.foundation:foundation-layout:1.8.2")
     implementation("androidx.constraintlayout:constraintlayout:2.2.1")
     implementation("androidx.core:core-ktx:1.16.0")
     implementation("androidx.preference:preference:1.2.1")
     implementation("be.mygod.librootkotlinx:librootkotlinx:1.2.1")
-    implementation("com.android.billingclient:billing-ktx:7.1.1")
     implementation("com.google.android.gms:play-services-oss-licenses:17.1.0")
     implementation("com.google.android.material:material:1.12.0")
     implementation("com.google.firebase:firebase-analytics:22.4.0")
-    implementation("com.google.firebase:firebase-crashlytics:19.4.3")
+    implementation("com.google.firebase:firebase-crashlytics:19.4.4")
     implementation("com.jakewharton.timber:timber:5.0.1")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test:runner:1.6.2")
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
-    id("com.android.application") version "8.10.0" apply false
+    id("com.android.application") version "8.10.1" apply false
     id("com.github.ben-manes.versions") version "0.52.0"
-    id("com.google.firebase.crashlytics") version "3.0.3" apply false
+    id("com.google.firebase.crashlytics") version "3.0.4" apply false
     id("com.google.gms.google-services") version "4.4.2" apply false
     alias(libs.plugins.aboutLibraries) apply false
     alias(libs.plugins.kotlin.android) apply false
