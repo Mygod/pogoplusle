d7d52a01413816e1f53a0ff08857eb27a3a515d7
Mygod <contact-git@mygod.be>
2026-01-18 21:36:36 -0800

    Update dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,7 +4,6 @@ plugins {
     id("com.android.application")
     id("com.google.gms.google-services")
     id("com.google.firebase.crashlytics")
-    kotlin("android")
     id("kotlin-parcelize")
     alias(libs.plugins.aboutLibraries)
     alias(libs.plugins.kotlin.compose)
@@ -19,8 +18,8 @@ android {
         applicationId = "be.mygod.pogoplusplus"
         minSdk = 28
         targetSdk = 36
-        versionCode = 40
-        versionName = "1.3.12"
+        versionCode = 41
+        versionName = "1.3.13"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         androidResources.localeFilters += listOf("en", "zh-rCN", "zh-rTW")
     }
@@ -56,7 +55,7 @@ dependencies {
     implementation(libs.material3.android)
     implementation("androidx.activity:activity-compose:1.12.2")
     implementation("androidx.browser:browser:1.9.0")
-    implementation("androidx.compose.foundation:foundation-layout:1.10.0")
+    implementation("androidx.compose.foundation:foundation-layout:1.10.1")
     implementation("androidx.constraintlayout:constraintlayout:2.2.1")
     implementation("androidx.core:core-ktx:1.17.0")
     implementation("androidx.preference:preference:1.2.1")
@@ -64,7 +63,7 @@ dependencies {
     implementation("com.google.android.gms:play-services-oss-licenses:17.3.0")
     implementation("com.google.android.material:material:1.13.0")
     implementation("com.google.firebase:firebase-analytics:23.0.0")
-    implementation("com.google.firebase:firebase-crashlytics:20.0.3")
+    implementation("com.google.firebase:firebase-crashlytics:20.0.4")
     implementation("com.jakewharton.timber:timber:5.0.1")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test:runner:1.7.0")
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,8 @@
 plugins {
-    id("com.android.application") version "8.13.2" apply false
+    id("com.android.application") version "9.0.0" apply false
     id("com.github.ben-manes.versions") version "0.52.0"
     id("com.google.firebase.crashlytics") version "3.0.6" apply false
     id("com.google.gms.google-services") version "4.4.4" apply false
     alias(libs.plugins.aboutLibraries) apply false
-    alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
 }
